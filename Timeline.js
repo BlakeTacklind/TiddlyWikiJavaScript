9f0b936e1e56936e434cbfc447de12e0b3b8caf8
Blake Tacklind <blake@tacklind.com>
2018-08-29 01:18:54 -0700

    significant improvements to structure and beginings of calendar reading

--- Timeline.js
+++ Timeline.js
@@ -169,20 +169,138 @@ function printTimeline(timeline, type){
 
 var CURRENT_TIME = null;
 
-/*
-Run the macro
-*/
-exports.run = function(type) {
-	if (!type) type = "PCC"
-	var output = "";
-	var timeStampRegEx = /<<TimeStamp\s+"([^"]+)"\s+"([^"]+)"(?:\s+"[^"]+")?\s*>>/g;
-	var eventObjects = new Array();
-	this.wiki.each(function(tiddler, title){
-		if (title == "Now"){
-			CURRENT_TIME = convert_to_gt(tiddler.getFieldString("time"))
+class Calendar{
+	constructor(abbreviation, json_data){
+		this.valid = false;
+
+		//Get the calendars name
+		if (json_data.name == undefined || json_data.name == "") {
+			console.log("Calendar doesn't have a name, giving it the abbreviation "+abbreviation);
+			this.name = abbreviation;
+		}
+		else {this.name = json_data.name};
+
+		//Set up reference date
+		if (json_data.ref != undefined && json_data.ref != ""){
+			var split_date = json_data.ref.split(" ");
+
+			this.ref_cal = split_date[1];
+			this.ref_date = split_date[0];
+		}
+		else{
+			//Only god time should not have a refrence point.
+			if (abbreviation != "GT"){
+				console.log("Calendar "+this.name +" doesn't have a start date");
+				return;
+			}
+
+			this.ref_cal = "GT";
+			this.ref_date = "0";
+			this.reference = 0;
 		}
 
-		if (!title.includes("$")){
+		//get start date
+		if (json_data.year_len == undefined || isNaN(json_data.year_len)){
+			console.log("Calendar "+this.name+" doesn't have a number of days");
+			return;
+		}
+		this.year_len = json_data.year_len;
+
+		//Get month by month data
+		if (json_data.n_months != json_data.months.length) {
+			console.log("Calendar "+this.name+" doesn't have the right number of months");
+			return;
+		}
+		this.months = new Array();
+		var pos = 0;
+		for (var i = 0; i < json_data.n_months; i++) {
+			this.months.push({name:json_data.months[i], length:json_data.month_len[json_data.months[i]], position:pos});
+			pos += json_data.month_len[json_data.months[i]];
+		}
+		if (pos != this.year_len){
+			console.log("Calendar "+this.name+" has "+pos+" days worth of months and has "+this.year_len+" days in a year");
+			return;
+		}
+
+		this.valid = true;
+	}
+
+	//Parse date like Y/M/D
+	parseDate(date_string){
+		if (this.reference == undefined){
+			console.log("refrence not defined for "+this.name+" Calendar");
+			return -1;
+		}
+
+		var date_split = date_string.split("/");
+
+		var time_gt;
+		switch(date_split.length){
+			case 1:
+				time_gt = parseInt(date_split[0]) * this.year_len + this.reference;
+				 break;
+			case 2:
+				time_gt = parseInt(date_split[0] * this.year_len) + parseInt(date_split[1]);
+				 break;
+			case 3:
+				time_gt = parseInt(date_split[0] * this.year_len) +
+				 this.months[parseInt(date_split[1])].position +
+				 parseInt(date_split[2]);
+				 break;
+			default:
+				console.log("\""+date_string+"\" is a bad date parse for Calendar "+this.name);
+				return -1;
+		}
+
+		if (isNaN(time_gt)){
+			console.log("\""+date_string+"\" did not produce a number for Calendar "+this.name);
+			return -1;
+		}
+
+		return time_gt;
+	}
+
+	get_string(god_time){
+
+	}
+}
+
+var Calendar_List;
+
+function GetInitalData(wiki){
+	//Get the time of Now
+	CURRENT_TIME = convert_to_gt(wiki.getTiddler("Now").fields.time);
+
+	//Get calendar data
+	const CALENDAR_START_STRING = "$:/Calendar/Custom/";
+
+	//Parse all calendars in wiki
+	Calendar_List = new Array();
+	wiki.each(function(tiddler, title){
+		if(title.startsWith(CALENDAR_START_STRING)){
+			var json_obj = JSON.parse(tiddler.fields.text);
+
+			var tmp = new Calendar(title.substring(CALENDAR_START_STRING.length), json_obj);
+
+			if (tmp.valid){
+				Calendar_List.push(tmp);
+			}
+			else{
+				console.log("Bad Calendar");
+			}
+		}
+	});
+
+
+}
+
+function GetEventObjects(wiki){
+
+	var timeStampRegEx = /<<TimeStamp\s+"([^"]+)"\s+"([^"]+)"(?:\s+"[^"]+")?\s*>>/g;
+	var eventObjects = new Array();
+
+	wiki.each(function(tiddler, title){
+		if (!title.startsWith("$")){
 			if (hasField(tiddler, "time")){
 				if (hasField(tiddler, "end")){
 					eventObjects.push(new EventObject(tiddler.getFieldString("time"), "start of "+title, title));
@@ -214,6 +332,23 @@ exports.run = function(type) {
 		}
 
 	});
+
+	return eventObjects;
+}
+
+/*
+Run the macro
+*/
+exports.run = function(type) {
+	//Satements for debug
+	console.log(this.wiki);
+	console.log(this.wiki.getTiddler("Now"));
+
+	if (!type) type = "PCC"
+
+	GetInitalData(this.wiki);
+
+	var eventObjects = GetEventObjects(this.wiki);
 
 	eventObjects = eventObjects.sort(compareTimeObjects);
 	return printTimeline(eventObjects, type);
